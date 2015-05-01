c50d6607eb7ca1b983520c638c25b32f3cdccf93
step5 <kevin00050@gmail.com>
2015-05-01 15:04:53 +0800

    Al

--- codes/profiles/test_blocks2.py
+++ codes/profiles/test_blocks2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import theano.tensor as T
 from theano import config, scan
-from blocks.bricks import MLP, Rectifier, Softmax
+from blocks.bricks import MLP, Rectifier, Softmax, Linear
 from blocks.bricks.recurrent import *
 from blocks.initialization import IsotropicGaussian, Constant
 import fuel, blocks
@@ -11,12 +11,13 @@ from fuel.schemes import SequentialScheme, ShuffledScheme
 from blocks.initialization import IsotropicGaussian, Constant
 from blocks.algorithms import *
 from blocks.bricks import WEIGHT
-from blocks.roles import INPUT, DROPOUT
+from blocks.roles import INPUT, DROPOUT, PARAMETER, OUTPUT
 from blocks.filter import VariableFilter
 from blocks.bricks.cost import CategoricalCrossEntropy, MisclassificationRate
 from blocks.graph import ComputationGraph, apply_dropout
 from blocks.main_loop import MainLoop
 from blocks.extensions import FinishAfter, Printing
+from blocks.extensions.plot import Plot
 from blocks.extensions.monitoring import DataStreamMonitoring
 from os.path import join as pjoin
 from settings import *
@@ -29,18 +30,63 @@ class Executor:
     def start(self):
         x = T.matrix('features', config.floatX)
         y = T.imatrix('targets')
-        mlp = MLP(activations = [
-            Rectifier(name='r0'), 
-            Rectifier(name='r1'), 
-            Rectifier(name='r2'), 
-            # Rectifier(name='r3'), 
-            Softmax(name='rs')
-        ],
-             dims=[108*7, 200, 200, 200, 48], weights_init=IsotropicGaussian(std=0.05, mean=0), biases_init=IsotropicGaussian(std=0.1))
-        # mlp = SimpleRecurrent(dim=200, activation=Rectifier(), weights_init=IsotropicGaussian(std=0.1))
-        y_hat = mlp.apply(x)
-        # y_hat = Softmax().apply(mlp.apply(x))
+
+        DIMS = [108*5, 2000, 2000, 2000, 2000, 2000, 48]
+        FUNCS = [
+            Rectifier(), 
+            Rectifier(), 
+            Rectifier(), 
+            Rectifier(), 
+            Rectifier(), 
+            Softmax()
+        ]
+
+        def lllistool(i, inp, func):
+            l = Linear(input_dim=DIMS[i], output_dim=DIMS[i+1], 
+                       weights_init=IsotropicGaussian(std=DIMS[i]**(-0.5)), 
+                       biases_init=IsotropicGaussian(std=DIMS[i]**(-0.5)),
+                       name='Lin{}'.format(i))
+            l.initialize()
+            func.name='Fun{}'.format(i)
+            return func.apply(l.apply(inp))
+
+        oup = x
+        for i in range(len(DIMS)-1):
+            oup = lllistool(i, oup, FUNCS[i])
+        y_hat = oup
+
         cost = CategoricalCrossEntropy().apply(y.flatten(), y_hat).astype(config.floatX)
+
+        cg = ComputationGraph(cost)
+        ips = VariableFilter(roles=[INPUT])(cg.variables)
+        ops = VariableFilter(roles=[OUTPUT])(cg.variables)
+        cg = apply_dropout(cg, ips[0:2:1], 0.2)
+        cg = apply_dropout(cg, ips[2:-2:1], 0.5)
+        cost = cg.outputs[0]
+
+        cost.name = 'cost'
+
+
+        Ws = VariableFilter(roles=[WEIGHT])(cg.variables)
+        norms = sum(w.norm(2) for w in Ws)
+        norms.name = 'norms'
+        path = pjoin(PATH['fuel'], 'train.hdf5')
+        # data = H5PYDataset(path, which_set='train', load_in_memory=True, subset=slice(0, 100000))
+        data = H5PYDataset(path, which_set='train', load_in_memory=True)
+        data_v = H5PYDataset(pjoin(PATH['fuel'], 'validate.hdf5'), which_set='validate', load_in_memory=True)
+        num = data.num_examples
+        data_stream = DataStream(data, iteration_scheme=ShuffledScheme(
+                        num, batch_size=128))
+        data_stream_v = DataStream(data_v, iteration_scheme=SequentialScheme(
+                        data_v.num_examples, batch_size=128))
+
+        cg = ComputationGraph(cost)
+        ips = VariableFilter(roles=[INPUT])(cg.variables)
+        ops = VariableFilter(roles=[OUTPUT])(cg.variables)
+        cg = apply_dropout(cg, ips[0:2:1], 0.2)
+        cg = apply_dropout(cg, ips[2:-2:1], 0.5)
+        cost = cg.outputs[0]
+
         cost.name = 'cost'
 
         mps = theano.shared(np.array([ph2id(ph48239(id2ph(t))) for t in range(48)]))
@@ -55,32 +101,28 @@ class Executor:
         lost23 = T.neq(y_hat39, y39).astype(config.floatX) / y39.shape[0]
         #lost23 = MisclassificationRate().apply(y39, y_hat39).astype(config.floatX)
         lost23.name = '2/3 loss'
-        cg = ComputationGraph(cost)
 
-        # inputs = VariableFilter(roles=[WEIGHT])(cg.variables)
-        # cg = apply_dropout(cg, inputs, 0.5)
-        # cost = cg.outputs[0]
 
         Ws = VariableFilter(roles=[WEIGHT])(cg.variables)
         norms = sum(w.norm(2) for w in Ws)
         norms.name = 'norms'
-        mlp.initialize()
         path = pjoin(PATH['fuel'], 'train.hdf5')
-        data = H5PYDataset(path, which_set='train', load_in_memory=True, subset=slice(0, 100))
-        # data = H5PYDataset(path, which_set='train', load_in_memory=Tr5e)
+        # data = H5PYDataset(path, which_set='train', load_in_memory=True, subset=slice(0, 100000))
+        data = H5PYDataset(path, which_set='train', load_in_memory=True)
         data_v = H5PYDataset(pjoin(PATH['fuel'], 'validate.hdf5'), which_set='validate', load_in_memory=True)
         num = data.num_examples
         data_stream = DataStream(data, iteration_scheme=ShuffledScheme(
                         num, batch_size=128))
         data_stream_v = DataStream(data_v, iteration_scheme=SequentialScheme(
                         data_v.num_examples, batch_size=128))
-        algo = GradientDescent(cost=cost, params=cg.parameters, step_rule=CompositeRule([Momentum(0.001, 0.9)]))
+        algo = GradientDescent(cost=cost, params=cg.parameters, step_rule=CompositeRule([Momentum(0.002, 0.9)]))
         monitor = DataStreamMonitoring( variables=[cost, lost01, norms],
                 data_stream=data_stream)
         monitor_v = DataStreamMonitoring( variables=[lost23],
                 data_stream=data_stream_v)
+        plt = Plot('Dalpha', channels=[['0/1 loss', '2/3 loss']], after_epoch=True)
         main_loop = MainLoop(data_stream = data_stream, 
                 algorithm=algo, 
-                extensions=[monitor, monitor_v, FinishAfter(after_n_epochs=2000), Printing()])
+                extensions=[monitor, monitor_v, FinishAfter(after_n_epochs=2000), Printing(), plt])
         
         main_loop.run()
