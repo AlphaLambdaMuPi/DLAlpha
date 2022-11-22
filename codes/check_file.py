e192eb662f2062dd3e024612e4e6d332a5c9028b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-22 15:59:09 +0000

    Adding tarfile member sanitization to extractall()

--- codes/check_file.py
+++ codes/check_file.py
@@ -28,7 +28,29 @@ def check_file():
         try:
             with tarfile.open(TAR_FILE) as tar:
                 data_dir = os.path.dirname(__file__)
-                tar.extractall(path=DATA_DIR)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=DATA_DIR)
         except OSError:
             logger.error('File {} not found, please download' 
                     'from kaggle and extract it here'.format(os.path.abspath(TAR_FILE)))
